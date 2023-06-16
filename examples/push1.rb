da531125f1c55be0ee4538e38eee618d49ce8068
leandrosardi <leandro.sardi@expandedventure.com>
2023-06-16 16:10:31 -0300

    coding

--- examples/push1.rb
+++ examples/push1.rb
@@ -4,7 +4,8 @@ require_relative '../config.rb'
 l = BlackStack::Workmesh.logger
 
 l.logs 'Loading order... '
-o = BlackStack::Scraper::Order.where(:node_for_micro_dfyl_appending=>'local', :micro_emails_delivery_push_success=>nil).first
+#o = BlackStack::Scraper::Order.where(:node_for_micro_dfyl_appending=>'local', :micro_emails_delivery_push_success=>nil).first
+o = BlackStack::Scraper::Order.where(:id=>'004d1c05-1190-4441-a680-9c865f8bc4ab').first
 l.logf o.id.green
 
 l.logs 'Loading node... '
